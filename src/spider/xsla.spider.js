dcb50660fa8fdc0b37fbd8a52853cea06c6d4bfe
czq <chenzhengqiang@youzan.com>
2018-12-11 13:09:54 +0800

    fix: conflict

--- src/spider/xsla.spider.js
+++ src/spider/xsla.spider.js
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-import Spider from './spider';
-import iconv from 'iconv-lite';
-import { crawlPage } from '../utils';
-
-class XslaSpider extends Spider {
-  constructor() {
-    super('xsla', 'https://www.xs.la', 'utf-8', 1);
-  }
-
-  fetch = async (url) => {
-    let { err, data } = await crawlPage(url);
-
-    if (!err && data != '') {
-      data = iconv.decode(data, this.decode);
-      let needJmp = /location.href=\'(.*)\';/g.exec(data);
-      if (needJmp !== null) {
-        await fetch(needJmp[1]);
-      } else {
-        let novel = this.getNovel(data);
-        novel != null && console.log(`[info]xs.la | ${novel.name} ${novel.url}`);
-      }
-    } else {
-      console.log('[info]biqu.cm | Jump!   1');
-    }
-  }
-}
-
-export default XslaSpider;
-=======
 const iconv = require('iconv-lite');
 const cheerio = require('cheerio');
 const queue = require('async/queue');
@@ -105,4 +75,3 @@ async function fetchList(url) {
 }
 
 exports.start = start;
->>>>>>> 061f3d5235d0ce290ba4917fb944de40a8b613fe
