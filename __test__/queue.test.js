3bc3f4c15bc15b5e1132e0e342162fa1b1d48d89
czq <chenzhengqiang@youzan.com>
2020-01-24 12:26:19 +0800

    fix: update third-party

--- __test__/queue.test.js
+++ __test__/queue.test.js
@@ -1,11 +1,21 @@
 const Queue = require('../src/third-party/queue');
 
 const queue = new Queue(5);
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+queue.work = async item => {
+  const dly = (Math.random() * 3) >>> 0;
+  await delay(dly * 1000);
+  console.log('当前数字', item, 'sleep:', dly);
+};
+
 queue.drain = () => console.log('drain');
+
 for (let i = 0; i < 100; i++) {
   queue.push(i);
 }
 
-setTimeout(() => {
-  queue.kill();
-}, 10000);
+// setTimeout(() => {
+//   queue.kill();
+// }, 10000);
